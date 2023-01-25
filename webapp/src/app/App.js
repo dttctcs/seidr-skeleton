978f4de67b40605423fe3359605d7ef03191e8c5
Mohr, Thomas (Datatactics) <t.mohr@datatactics.de>
2023-01-25 10:27:45 +0100

    Removed obsolete MantineProvider
    * SeidrProvider does already include a MantineProvider

--- webapp/src/app/App.js
+++ webapp/src/app/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { MantineProvider } from '@mantine/core';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import Demo from 'features/demo/Demo'
 import MainFrame from '../common/MainFrame';
 import LoginPage from 'features/auth/user/LoginPage';
@@ -8,19 +7,13 @@ import LoginPage from 'features/auth/user/LoginPage';
 
 function App() {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      withCSSVariables
-    >
-      <Routes>
-        <Route exact={true} path="/login" element={<LoginPage />} />
-        <Route path="/" element={<MainFrame />}>
-          <Route path="/" element={<Demo />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </MantineProvider>
+    <Routes>
+      <Route exact={true} path="/login" element={<LoginPage/>}/>
+      <Route path="/" element={<MainFrame/>}>
+        <Route path="/" element={<Demo/>}/>
+      </Route>
+      <Route path="*" element={<Navigate to="/"/>}/>
+    </Routes>
   );
 }
 

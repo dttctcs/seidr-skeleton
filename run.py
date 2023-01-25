41967937923bdddb7ba24e0ec1d2210634d20d8c
Mohr, Thomas (Datatactics) <t.mohr@datatactics.de>
2023-01-25 10:24:21 +0100

    Added check for admin user creation

--- run.py
+++ run.py
@@ -1,6 +1,13 @@
 from app import app
 
-app.appbuilder.sm.add_user(
-    "admin", "admin", "admin", "admin@for.bar", app.appbuilder.sm.find_role("Admin"), "admin"
-)
+user = app.appbuilder.sm.find_user(username="admin")
+if not user:
+    app.appbuilder.sm.add_user(
+        username="admin",
+        first_name="admin",
+        last_name="admin",
+        email="admin@for.bar",
+        role=app.appbuilder.sm.find_role("Admin"),
+        password="admin"
+    )
 app.run(host="0.0.0.0", port=6060, debug=True)

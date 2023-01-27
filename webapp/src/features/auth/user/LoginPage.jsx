118cf9ba71ab92c64a3922801f0666a9c2c30f13
Mohr, Thomas (Datatactics) <t.mohr@datatactics.de>
2023-01-27 14:40:14 +0100

    Simplified login page

--- webapp/src/features/auth/user/LoginPage.jsx
+++ webapp/src/features/auth/user/LoginPage.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 import { useSeidrAuth } from 'seidr-react';
 import { useForm } from '@mantine/form';
-import { Alert, Box, Button, Group, Paper, PasswordInput, Stack, Text, TextInput } from '@mantine/core';
+import { Alert, Button, Card, Flex, PasswordInput, Stack, TextInput, Title } from '@mantine/core';
 
 export default function LoginPage(props) {
   const passwordMinLength = 5;
@@ -12,34 +12,24 @@ export default function LoginPage(props) {
       username: '',
       password: '',
     },
-
     validate: {
       password: (val) => (val.length < passwordMinLength ? `Password should include at least ${passwordMinLength} characters` : null),
     },
   });
 
-  const onSubmit = (data) => {
-    signin(data);
-  };
+  const onSubmit = (data) => signin(data);
 
-  if (user) {
-    return <Navigate to='/' />;
-  }
+  if (user) return <Navigate to='/' />;
 
   return (
-    <Box
-      sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}
+    <Flex
+      h='100vh'
+      justify='center'
+      align='center'
+      direction='column'
     >
-      <Box sx={{ display: 'flex', alignItems: 'center', paddingBottom: 20 }}>
-        <Text size='xl' weight={500}>
-          {process.env.REACT_APP_NAME} Login
-        </Text>
-      </Box>
-      <Paper radius='md' p='xl' withBorder {...props}>
-        <Text size='lg' weight={500}>
-          LOGIN
-        </Text>
-
+      <Title p='md'>{process.env.REACT_APP_NAME}</Title>
+      <Card radius='md' p='xl' withBorder {...props}>
         <form onSubmit={form.onSubmit(onSubmit)}>
           <Stack>
             <TextInput
@@ -50,7 +40,6 @@ export default function LoginPage(props) {
               onChange={(event) => form.setFieldValue('username', event.currentTarget.value)}
               error={form.errors.username && 'Invalid username'}
             />
-
             <PasswordInput
               required
               label='Password'
@@ -59,20 +48,11 @@ export default function LoginPage(props) {
               onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
               error={form.errors.password && `Password should include at least ${passwordMinLength} characters`}
             />
+            {error ? (<Alert title='Login failed' color='red'>{error}</Alert>) : null}
+            <Button type='submit' loading={loading} fullWidth>Login</Button>
           </Stack>
-
-          <Group position='apart' mt='xl'>
-            {error ? (
-              <Alert sx={{ flex: 1 }} title='Bummer!' color='red'>
-                {error}
-              </Alert>
-            ) : null}
-            <Button type='submit' loading={loading} fullWidth>
-              Login
-            </Button>
-          </Group>
         </form>
-      </Paper>
-    </Box>
+      </Card>
+    </Flex>
   );
 }
